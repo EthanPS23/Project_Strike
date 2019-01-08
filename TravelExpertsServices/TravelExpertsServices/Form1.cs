e2fdd806244a337b82b1fe2c4a9cc24dda569c2f
EthanPS23 <ethanps23@gmail.com>
2019-01-08 13:22:09 -0700

    Update Form1.cs

--- TravelExpertsServices/TravelExpertsServices/Form1.cs
+++ TravelExpertsServices/TravelExpertsServices/Form1.cs
@@ -19,38 +19,83 @@ namespace TravelExpertsServices
 
         private void btnAddPkg_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the packages pages and changes to the packages page
             tabControl1.SelectedIndex = 1;
             btnAddEditPkg.Text = "Add Package";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the packages pages and changes to the packages page
             tabControl1.SelectedIndex = 1;
             btnAddEditPkg.Text = "Edit Package";
         }
 
         private void btnAddProd_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the products pages and changes to the products page
             tabControl1.SelectedIndex = 2;
             btnAddEditProd.Text = "Add Product";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the products pages and changes to the products page
             tabControl1.SelectedIndex = 2;
             btnAddEditProd.Text = "Edit Product";
         }
 
         private void btnAddSup_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the suppliers pages and changes to the suppliers page
             tabControl1.SelectedIndex = 3;
             btnAddEditSup.Text = "Add Supplier";
         }
 
         private void btnEditSup_Click(object sender, EventArgs e)
         {
+            // changes the text for the add/edit button on the packages pages and changes to the packages page
             tabControl1.SelectedIndex = 3;
             btnAddEditSup.Text = "Edit Supplier";
+        }
+
+        private void btnAddEditPkg_Click(object sender, EventArgs e)
+        {
+            // Checks the text of the Add/edit package button in order to perform various logic
+            if (btnAddEditPkg.Text=="Add Package")
+            {
+
+            }
+            else if (btnAddEditPkg.Text=="Edit Package")
+            {
+
+            }
+        }
+
+        private void btnAddEditProd_Click(object sender, EventArgs e)
+        {
+            // Checks the text of the Add/edit product button in order to perform various logic
+            if (btnAddEditProd.Text == "Add Product")
+            {
+
+            }
+            else if (btnAddEditProd.Text == "Edit Product")
+            {
+
+            }
+        }
+
+        private void btnAddEditSup_Click(object sender, EventArgs e)
+        {
+            // Checks the text of the Add/edit supplier button in order to perform various logic
+            if (btnAddEditSup.Text == "Add Supplier")
+            {
+
+            }
+            else if (btnAddEditSup.Text == "Edit Supplier")
+            {
+
+            }
         }
     }
 }
